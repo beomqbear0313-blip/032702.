505ad80bfbeb040700f3afb6b94ee11c587fe775
眼神空洞的大學生 <beomqbear0313@gmail.com>
2026-03-27 11:11:28 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Manager
+{
+	public readonly DateTime Startup;
+
+	public Manager()
+	{
+		Startup = DateTime.Now;
+	}
+
+	public DateTime GetStartup()
+	{
+		return Startup;
+	}
+	
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03272
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m = new Manager();
+
+            Console.WriteLine( m.GetStartup().ToString() );
+
+           // m.Startup = DateTime.UtcNow;
+
+            Console.WriteLine( m.GetStartup().ToString());
+        }
+    }
+}
